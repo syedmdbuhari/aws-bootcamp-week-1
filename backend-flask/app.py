526e604f61b415c7e053431efc4eedfe281c3b7c
Syed Buhari <sbuhari@cisco.com>
2023-12-30 08:05:02 +0530

    rollbar support

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -22,9 +22,14 @@ from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExport
 from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.export import BatchSpanProcessor
 
-# X-RAY
-from aws_xray_sdk.core import xray_recorder
-from aws_xray_sdk.ext.flask.middleware import XRayMiddleware
+# # X-RAY
+# from aws_xray_sdk.core import xray_recorder
+# from aws_xray_sdk.ext.flask.middleware import XRayMiddleware
+
+# Rollbar
+import rollbar
+import rollbar.contrib.flask
+from flask import got_request_exception
 
 # Cloudwatch logging
 # import watchtower
@@ -59,6 +64,23 @@ RequestsInstrumentor().instrument()
 # xray_recorder.configure(service='awsbootcamp', dynamic_naming=xray_url)
 # XRayMiddleware(app, xray_recorder)
 
+# Rollbar init
+rollbar_access_token = os.getenv('ROLLBAR_ACCESS_TOKEN')
+# @app.before_first_request
+# def init_rollbar():
+    # """init rollbar module"""
+rollbar.init(
+    # access token
+    rollbar_access_token,
+    # environment name
+    'production',
+    # server root directory, makes tracebacks prettier
+    root=os.path.dirname(os.path.realpath(__file__)),
+    # flask already sets up logging
+    allow_logging_basic_config=False)
+
+# send exceptions from `app` to rollbar, using flask's signal system.
+got_request_exception.connect(rollbar.contrib.flask.report_exception, app)
 
 frontend = os.getenv('FRONTEND_URL')
 backend = os.getenv('BACKEND_URL')
@@ -76,6 +98,12 @@ cors = CORS(
 #     timestamp = strftime('[%Y-%b-%d %H:%M]')
 #     LOGGER.error('%s %s %s %s %s %s', timestamp, request.remote_addr, request.method, request.scheme, request.full_path, response.status)
 #     return response
+
+
+@app.route('/rollbar/test')
+def rollbar_test():
+    rollbar.report_message('Hello World!', 'warning')
+    return "Hello World!"
 
 @app.route("/api/message_groups", methods=['GET'])
 def data_message_groups():
